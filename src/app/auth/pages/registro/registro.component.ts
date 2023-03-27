99cd9688936ef1849b0c8138156c4b543b502a5d
Jeff Worker <>
2023-03-27 16:24:32 +0200

    ASync validators

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { emailPattern, nombreApellidoPattern, nopuedeSerStrider } from 'src/app/shared/validators/validaciones';
+// import { emailPattern, nombreApellidoPattern, nopuedeSerStrider } from 'src/app/shared/validators/validaciones'; // de cuando lo cogiamos del otro archivo que no era un servicio
 import { ValidatorService } from 'src/app/shared/validators/validator.service';
+import { EmailValidatorService } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-registro',
@@ -14,7 +15,7 @@ export class RegistroComponent implements OnInit {
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [ Validators.required, Validators.pattern( this.vs.nombreApellidoPattern ) ] ],
     // email: ['', [ Validators.required, Validators.email ]] //esta validacion es la de html que no es muy allá
-    email: ['', [ Validators.required, Validators.pattern( this.vs.emailPattern ) ]],
+    email: ['', [ Validators.required, Validators.pattern( this.vs.emailPattern ) ], [ this.emailValidator ] ],
     username: ['', [ Validators.required, this.vs.nopuedeSerStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]]
@@ -22,8 +23,23 @@ export class RegistroComponent implements OnInit {
     validators: [ this.vs.camposIguales('password', 'password2')]
   }) 
 
+  get emailErrorMsg(): string {
+    const errors = this.miFormulario.get('email')?.errors;
+
+    if( errors?.['required'] ) {
+      return 'Email es obligatorio.';
+    } else if( errors?.['pattern']) {
+      return 'El valor debe tener formato de email.'
+    } else if( errors?.['emailTomado']) {
+      return 'El email ya se ha utilizado.'
+    }
+    return '';
+
+  }  
+
   constructor( private fb: FormBuilder,
-               private vs: ValidatorService ) {
+               private vs: ValidatorService,
+               private emailValidator: EmailValidatorService ) {
 
   }
 
@@ -31,12 +47,29 @@ export class RegistroComponent implements OnInit {
     this.miFormulario.reset({
       nombre:'Jorge Ll',
       email: 'test1@test.com',
-      username: 'Mmmaksmds'
+      username: 'Mmmaksmds',
+      password: '123456',
+      password2: '123456'
     })
   }
   campoNoValido( campo: string ){
     return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched
   }
+
+  // emailRequired(){  //ESTOS METODOS SON MUY REPETITIVOS
+  //   return this.miFormulario.get('email')?.errors?.['required'] 
+  //           && this.miFormulario.get('email')?.touched;
+  // }
+
+  // emailFormato(){
+  //   return this.miFormulario.get('email')?.errors?.['pattern']
+  //           && this.miFormulario.get('email')?.touched;
+  // }
+
+  // emailTomado(){
+  //   return this.miFormulario.get('email')?.errors?.['emailTomado']
+  //           && this.miFormulario.get('email')?.touched;
+  // }
 
   submitFormulario() {
     console.log(this.miFormulario.value);
